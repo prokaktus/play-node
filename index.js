e53c7a0b2afe4ccb9ced63aad5e45146d63e5272
prokaktus <proktusfahitasiv@gmail.com>
2016-03-04 20:28:58 +0300

    Adding hbs template engine

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 'use strict';
 
+const path = require('path');
+const express = require('express');
+const raven = require('raven');
+const hbs = require('hbs');
+const layouts = require('handlebars-layouts');
+const handlebars = require('handlebars');
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
 // setup loggers
 require('./lib/setup_logger')();
 const logger = require('./lib/get_logger')('default', __filename);
-const raven = require('raven');
-const express = require('express');
-
-
-require('dotenv').config();
 
 require('./lib/start')();
 
 logger.log('info', 'Hi');
 
 const app = express();
-const port = process.env.PORT;
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'frontend', 'views'));
+
+hbs.registerHelper(layouts(handlebars));
+hbs.registerPartials(path.join(__dirname, 'frontend', 'views', 'partials'));
+
+app.use('/static', express.static('public'));
 app.get('/', function (req, res) {
     res.send('Hello world!');
+});
+
+app.get('/hbs', function (req, res) {
+    res.render('index');
 })
 
-app.listen(port, function () {
-    logger.info('Start listen on port ' + port);
+app.listen(PORT, function () {
+    logger.info('Start listen on port ' + PORT);
 });
