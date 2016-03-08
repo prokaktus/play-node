bf83da5cf2734718dc7cf17e2401f5d9d6e5a402
prokaktus <proktusfahitasiv@gmail.com>
2016-03-08 06:21:12 +0300

    Add migrations and factories

---
+++ migrations/sqls/20160307210209-initial-up.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+create table posts(
+    ID uuid primary key not null,
+    content text,
+    created timestamptz
+);
---
+++ tests/factories.es6
@@ -0,0 +1,7 @@
+import { Factory } from 'rosie';
+
+const Post = new Factory();
+
+const Author = new Factory();
+
+export {Post, Author};
